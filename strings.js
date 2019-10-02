9f9541eb3054760e1fbc719688d896e733a915f0
Vanessa Tsang <info@vanessatsang.uk>
2019-10-02 14:23:36 +0100

    Adding all 20 exercises to my Node School repro

--- strings.js
+++ strings.js
@@ -1,2 +1,6 @@
-const someString = 'this is a string'
-console.log(someString)
+
+// Declaring a variable named someString and assigning that a string.
+const someString = 'this is a string';
+
+// Will print out the value of the variabl someString.
+console.log(someString);
